e2019be84e7c8ef9facb6200f3a92b700f0c66ed
ryanwbachman <ryanwbachman@gmail.com>
2017-09-13 02:16:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
-//define functions here
+function getIt(){
+  $('p').on('click',function(){
+    alert('Hey!!');
+  });
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt(){
+  $(document).on('keydown',function(key){
+    if(key.which == 71){
+      alert("'g' was pressed!");
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit',function() {
+    // if ($( 'input:first' ).val() === 'correct') {
+    //   alert('Your form is going to be submitted now.');
+    //   return;
+    // }
+    alert('Your form is going to be submitted now.');
+    return;
+  });
+}
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
